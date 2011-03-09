d0a1dbab3b8de0aa5e426e4b28040821209ec8d9
f.fondement <devnull@localhost>
2011-03-09 12:22:10 +0100

    Some documentation

--- src/main/java/com/mt/storage/CloseableKeyIterator.java
+++ src/main/java/com/mt/storage/CloseableKeyIterator.java
@@ -2,6 +2,10 @@ package com.mt.storage;
 
 import java.util.Iterator;
 
+/**
+ * An iterator that should be closed once information is read.
+ * This is the return type of {@link Store#get(String, Constraint, int)}.
+ */
 public interface CloseableKeyIterator extends Iterator<String> {
 
 	public abstract void close();
