0d981b8c8ba22e6acf7c9b57d7220ba4e2bba791
Frédéric Fondement <frederic.fondement@uha.fr>
2011-03-14 10:28:12 +0100

    DatabaseNotReached declared as a soft exception

--- storage/src/main/java/com/googlecode/n_orm/DatabaseNotReachedException.java
+++ storage/src/main/java/com/googlecode/n_orm/DatabaseNotReachedException.java
@@ -8,7 +8,7 @@ import com.googlecode.n_orm.Incrementing;
  * This exception is thrown when a property (or an element of a {@link Map}) annotated with {@link Incrementing} decrements.
  *
  */
-public class DatabaseNotReachedException extends Exception {
+public class DatabaseNotReachedException extends RuntimeException {
 	private static final long serialVersionUID = -2698947883988453416L;
 
 	public DatabaseNotReachedException(Exception cause) {
