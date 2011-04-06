81f954b274050821dbc69b0635444f443dfec4ff
f.fondement <f.fondement@localhost>
2011-04-06 18:17:59 +0200

    first regressive attempt

---
+++ hbase/src/test/java/com/googlecode/n_orm/storage/HBaseLoaderTest.java
@@ -0,0 +1,57 @@
+package com.googlecode.n_orm.storage;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.apache.hadoop.conf.Configuration;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.googlecode.n_orm.hbase.Store;
+
+
+public class HBaseLoaderTest {
+	private String testPath = null;
+	
+	@Before
+	public void getResourcePath() {
+		if (testPath == null) {
+			String clFilename = HBaseLoaderTest.class.getName().replace('.', '/')+".class";
+			URL res = ClassLoader.getSystemClassLoader().getResource(clFilename);
+			File clFile = new File(clFilename);
+			File file = new File(res.getPath());
+			do {
+				clFile = clFile.getParentFile();
+				file = file.getParentFile();
+			} while (clFile != null);
+			testPath = file.getAbsolutePath();
+		}
+	}
+
+	@Test
+	public void loadProperties() throws IOException {
+		Configuration conf = Store.getStore(testPath + "/etc/conf1/," + testPath + "/etc/conf2," + testPath + "/etc/conf2/conf3/").getConf();
+		assertEquals("dummyval", conf.get("dummy.prop.one"));
+		assertEquals("2", conf.get("dummy.prop.two"));
+		assertEquals("dummyval12", conf.get("dummy12.prop"));
+		assertEquals("dummy2val", conf.get("dummy2.prop"));
+	}
+
+	@Test(expected=IOException.class)
+	public void loadPropertiesMissingHbaseSite() throws IOException {
+		Store.getStore(testPath + "/etc/conf1/," + testPath + "/etc/conf2");
+	}
+
+	@Test(expected=IOException.class)
+	public void loadPropertiesBadFolder() throws IOException {
+		Store.getStore(testPath + "/etcx/conf1/," + testPath + "/etc/conf2," + testPath + "/etc/conf2/conf3/");
+	}
+
+	@Test(expected=IOException.class)
+	public void loadPropertiesNoConf() throws IOException {
+		Store.getStore(testPath + "/etc/conf1/," + testPath + "/etc/conf4," + testPath + "/etc/conf2/conf3/");
+	}
+}
---
+++ storage/src/test/java/com/googlecode/n_orm/PersistableSearchTest.java
@@ -0,0 +1,170 @@
+package com.googlecode.n_orm;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.googlecode.n_orm.DatabaseNotReachedException;
+import com.googlecode.n_orm.Key;
+import com.googlecode.n_orm.Persisting;
+import com.googlecode.n_orm.PersistingElement;
+import com.googlecode.n_orm.StorageManagement;
+import com.googlecode.n_orm.storeapi.Constraint;
+
+
+public class PersistableSearchTest {
+	
+	public PersistableSearchTest() throws Exception {
+		StoreTestLauncher.registerStorePropertiesForInnerClasses(getClass());
+	}
+	
+	@Persisting(table="PersistableSearch")
+	public static class SUTClass {
+		@Key public int key1;
+		@Key(order=2) public int key2;
+		
+		public int dummyVar;
+		
+		public SUTClass(int key1, int key2) {
+			this.key1 = key1;
+			this.key2 = key2;
+		}
+		
+		public boolean equals(Object rhs) {
+			return rhs != null && rhs.getClass().equals(SUTClass.class) && ((SUTClass)rhs).key1 == this.key1 && ((SUTClass)rhs).key2 == this.key2;
+		}
+		
+		
+	}
+	
+	@Before public void createSuts() {
+		try {
+			if (new SUTClass(1, 1).existsInStore())
+				return;
+		} catch (Exception e1) {
+			throw new RuntimeException(e1);
+		}
+		SUTClass s;
+		for(int i = 0; i <= 100; ++i) {
+			for(int j = 0; j <= 10; ++j) {
+				s = new SUTClass(i, j);
+				s.dummyVar = i + j;
+				try {
+					s.store();
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
+		 
+		 //Simulates a new session by emptying elements cache
+		 KeyManagement.getInstance().cleanupKnownPersistingElements();
+	}
+
+	public void checkOrder(Set<? extends PersistingElement> elements) {
+		PersistingElement last = null;
+		 for (PersistingElement elt : elements) {
+			if (last != null) assertTrue(last.compareTo(elt) <= 0);
+			last = elt;
+		}
+	}
+	
+	@Test public void searchInexistingSutsWithNegativeFirstKey() throws DatabaseNotReachedException {
+		CloseableIterator<SUTClass> res = StorageManagement.findElement(SUTClass.class, new Constraint(SUTClass.class, null, "key1", -10, -5), 50, (String[])null);
+		assertFalse(res.hasNext());
+	}
+	
+	@Test public void searchInexistingSutsWithFirstKey() throws DatabaseNotReachedException {
+		CloseableIterator<SUTClass> res = StorageManagement.findElement(SUTClass.class, new Constraint(SUTClass.class, null, "key1", 150, null), 50, (String[])null);
+		assertFalse(res.hasNext());
+	}
+	
+	@Test public void searchSutsWithFirstKey() throws DatabaseNotReachedException {
+		Set<SUTClass> res = StorageManagement.findElementsToSet(SUTClass.class, new Constraint(SUTClass.class, null, "key1", 49, 55), 1000, (String[])null);
+		assertEquals((55-49+1)*11, res.size());
+		for (SUTClass sutClass : res) {
+			assertTrue(49 <= sutClass.key1 && sutClass.key1 <= 55);
+			assertEquals(0, sutClass.dummyVar);
+		}
+		checkOrder(res);
+	}
+	
+	@Test public void searchSutsWithFirstKeyAndActivate() throws DatabaseNotReachedException {
+		Set<SUTClass> res = StorageManagement.findElementsToSet(SUTClass.class, new Constraint(SUTClass.class, null, "key1", 49, 55), 1000, new String[0]);
+		assertEquals((55-49+1)*11, res.size());
+		for (SUTClass sutClass : res) {
+			assertTrue(49 <= sutClass.key1 && sutClass.key1 <= 55);
+			assertEquals(sutClass.key1+sutClass.key2, sutClass.dummyVar);
+		}
+		checkOrder(res);
+	}
+	
+	@Test public void search50SutsWithFirstKey() throws DatabaseNotReachedException {
+		Set<SUTClass> res = StorageManagement.findElementsToSet(SUTClass.class, new Constraint(SUTClass.class, null, "key1", 49, 55), 50, (String[])null);
+		assertEquals(50, res.size());
+		checkOrder(res);
+	}
+	
+	@Test public void search1SutWithFirstKey() throws DatabaseNotReachedException {
+		Set<SUTClass> ress = StorageManagement.findElementsToSet(SUTClass.class, new Constraint(SUTClass.class, null, "key1", 49, 55), 1, (String[])null);
+		assertEquals(1, ress.size());
+		SUTClass res = ress.iterator().next();
+		assertTrue(49 <= res.key1 && res.key1 <= 55);
+		assertEquals(0, res.dummyVar);
+	}
+	
+	@Test public void search1SutWithFixedFirstKey() throws DatabaseNotReachedException {
+		Set<SUTClass> ress = StorageManagement.findElementsToSet(SUTClass.class, new Constraint(SUTClass.class, null, "key1", 49, 49), 1, (String[])null);
+		assertEquals(1, ress.size());
+		SUTClass res = ress.iterator().next();
+		assertEquals(49, res.key1);
+	}
+	
+	@Test public void searchSutsWithSecondKey() throws DatabaseNotReachedException {
+		Map<String, Object> k1Val = new TreeMap<String, Object>();
+		k1Val.put("key1", 35);
+		Set<SUTClass> res = StorageManagement.findElementsToSet(SUTClass.class, new Constraint(SUTClass.class, k1Val, "key2", 5, 7), 1000, (String[])null);
+		List<Integer> toBeFound = Arrays.asList(new Integer [] {5, 6, 7});
+		assertEquals(3, res.size());
+		for (SUTClass ret : res) {
+			assertEquals(35, ret.key1);
+			assertTrue(toBeFound.contains(ret.key2));
+		}
+		checkOrder(res);
+	}
+	
+	@Test public void searchSutsWithSecondKeyNoUpper() throws DatabaseNotReachedException {
+		Map<String, Object> k1Val = new TreeMap<String, Object>();
+		k1Val.put("key1", 35);
+		Set<SUTClass> res = StorageManagement.findElementsToSet(SUTClass.class, new Constraint(SUTClass.class, k1Val, "key2", 5, null), 1000, (String[])null);
+		List<Integer> toBeFound = Arrays.asList(new Integer [] {5, 6, 7, 8, 9, 10});
+		assertEquals(6, res.size());
+		for (SUTClass ret : res) {
+			assertEquals(35, ret.key1);
+			assertTrue(toBeFound.contains(ret.key2));
+		}
+		checkOrder(res);
+	}
+	
+	@Test public void searchSutsWithSecondKeyNoLower() throws DatabaseNotReachedException {
+		Map<String, Object> k1Val = new TreeMap<String, Object>();
+		k1Val.put("key1", 35);
+		Set<SUTClass> res = StorageManagement.findElementsToSet(SUTClass.class, new Constraint(SUTClass.class, k1Val, "key2", null, 5), 1000, (String[])null);
+		List<Integer> toBeFound = Arrays.asList(new Integer [] {5, 4, 3, 2, 1, 0});
+		assertEquals(6, res.size());
+		for (SUTClass ret : res) {
+			assertEquals(35, ret.key1);
+			assertTrue(toBeFound.contains(ret.key2));
+		}
+		checkOrder(res);
+	}
+}
