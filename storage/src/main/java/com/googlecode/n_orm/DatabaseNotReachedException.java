f6b9bc2dc11e7134b91246dd2973c916779f624d
Fr?d?ric Fondement <frederic.fondement@uha.fr>
2011-05-03 09:34:43 +0200

    corrected some javadoc typos

--- storage/src/main/java/com/googlecode/n_orm/DatabaseNotReachedException.java
+++ storage/src/main/java/com/googlecode/n_orm/DatabaseNotReachedException.java
@@ -1,12 +1,8 @@
 package com.googlecode.n_orm;
 
-import java.util.Map;
-
-import com.googlecode.n_orm.Incrementing;
 
 /**
- * This exception is thrown when a property (or an element of a {@link Map}) annotated with {@link Incrementing} decrements.
- *
+ * A problem happened while querying the database. See {@link Throwable#getCause()} for details.
  */
 public class DatabaseNotReachedException extends RuntimeException {
 	private static final long serialVersionUID = -2698947883988453416L;
