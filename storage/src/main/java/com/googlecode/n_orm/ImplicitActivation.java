71b236c3e2ee15cec75d0e6b96dd95aca8651c57
Frédéric Fondement <frederic.fondement@uha.fr>
2011-05-03 09:34:43 +0200

    corrected some javadoc typos

--- storage/src/main/java/com/googlecode/n_orm/ImplicitActivation.java
+++ storage/src/main/java/com/googlecode/n_orm/ImplicitActivation.java
@@ -4,18 +4,19 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.Map;
+import java.util.Set;
 
 import com.googlecode.n_orm.ImplicitActivation;
 import com.googlecode.n_orm.PersistingElement;
 
+
 /**
- * Prevents automatic activation of a property (including keys).
- * Persisting properties (including keys) are automatically activated while using {@link PersistingElement#activate(String...)}.
- * By marking a property with this annotation, an activation will not automatically trigger an activation.
- * It is not useful to declare this annotation on a column family for they will not be automatically be activated anyway unless annotated with {@link ImplicitActivation}.
+ * Column families (i.e. properties of type {@link Map} or {@link Set}) must be activated explicitly using {@link PersistingElement#activate(String...)}.
+ * By marking a column family with this annotation, an activation will always trigger an activation. However, be careful with this annotation for all elements in the column family will be activated regardless their number.
+ * It is not useful to declare this annotation on a normal property or a key (including persisting properties or key) for they will be automatocally be activated.
  * @see PersistingElement#activate(String...)
  */
-
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface ImplicitActivation {
