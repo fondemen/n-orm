ef85fd25e8d886c116b6af6c5718754987729d69
frederic fondement <frederic.fondement@uha.fr>
2011-05-06 19:36:23 +0200

    Preparing java 7 ; makes possible to have a {{{try(CloseableIterator it = storageManagement.findElements().ofClass(Foo.class).iterate()) {	while it.hasNext() {...}} }}}

--- storage/src/main/java/com/googlecode/n_orm/CloseableIterator.java
+++ storage/src/main/java/com/googlecode/n_orm/CloseableIterator.java
@@ -1,5 +1,6 @@
 package com.googlecode.n_orm;
 
+import java.io.Closeable;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -7,7 +8,7 @@ import java.util.Set;
  * An iterator that should be closed once information is read.
  * This is the return type of {@link Store#get(String, Constraint, int, Set)}.
  */
-public interface CloseableIterator<T> extends Iterator<T> {
+public interface CloseableIterator<T> extends Iterator<T>, Closeable {
 
 	public abstract void close();
 }
