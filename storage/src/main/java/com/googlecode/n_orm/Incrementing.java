49c42e3112e3418d0bd3603a859c0f2d8c9ecf7e
f.fondement <f.fondement@localhost>
2011-06-06 16:28:47 +0200

    incrementing properties can decrement

--- storage/src/main/java/com/googlecode/n_orm/Incrementing.java
+++ storage/src/main/java/com/googlecode/n_orm/Incrementing.java
@@ -6,12 +6,12 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.util.Map;
 
-import com.googlecode.n_orm.DecrementException;
+import com.googlecode.n_orm.IncrementException;
 import com.googlecode.n_orm.Persisting;
 import com.googlecode.n_orm.PersistingElement;
 /**
  * This annotation may be placed either on a number property, or on a {@link Map} of numbers.
- * Such annotated properties are only allowed to increment over time, otherwise a {@link DecrementException} is thrown.
+ * By default, the property is only allowed to increment over time.
  * For maps, it's the element at a given key that must not decrement.
  * Regarding storage, it's not the actual value that is sent when invoking {@link PersistingElement#store()}, but the difference.
  * The situation is illustrated by the following example:<br><code>
@@ -25,4 +25,13 @@ import com.googlecode.n_orm.PersistingElement;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface Incrementing {
+	
+	public enum Mode {Free, IncrementOnly, DecrementOnly};
+	
+	/**
+	 * Whether the incrementing property is allowed to increment or decrement over time.
+	 * For non free modes, a {@link IncrementException} is thrown if constraint set by mode is not enforced.
+	 * As there is no mean to state a default value, {@link Mode#IncrementOnly} properties must be positive and {@link Mode#IncrementOnly} must be negative.
+	 */
+	Mode mode() default Mode.IncrementOnly;
 }
