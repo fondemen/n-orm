9bf65c52e72cd8345b4e645013d30b5fd39756d1
Frédéric Fondement <frederic.fondement@uha.fr>
2011-07-06 20:28:29 +0200

    Debugged non String SetColumnFamilies Added non string map tests

---
+++ storage/src/test/java/com/googlecode/n_orm/cf/SetIntTest.java
@@ -0,0 +1,49 @@
+package com.googlecode.n_orm.cf;
+
+import java.util.Set;
+
+import org.apache.commons.collections.set.AbstractTestSet;
+
+import com.googlecode.n_orm.Key;
+import com.googlecode.n_orm.Persisting;
+
+public class SetIntTest extends AbstractTestSet {
+	@Persisting
+	public static class Element {
+		@Key
+		public String key;
+		public SetColumnFamily<Integer> elements = new SetColumnFamily<Integer>();
+	}
+
+	Element element;
+	SetColumnFamily<Integer> aSet;
+
+	public SetIntTest(String name) {
+		super(name);
+	}
+
+	@Override
+	public Set<Integer> makeEmptySet() {
+		element = new Element();
+		element.key = "testelt";
+		aSet = element.elements;
+		return aSet;
+	}
+
+	@Override
+	public boolean isNullSupported() {
+		return false;
+	}
+
+	@Override
+	public Integer[] getFullNonNullElements() {
+		return new Integer[] { 0, 1, 3,
+				1, 7, 8, -9, 13, 14,
+				15 };
+	}
+
+	public Integer[] getOtherNonNullElements() {
+		return new Integer[] { 12, -12};
+	}
+
+}
