f4a26506d4aaa0759b4d11ba6219e2ef2344e00f
f.fondement <f.fondement@localhost>
2011-07-08 14:01:58 +0200

    repaired some invalid tests

--- sample/src/test/java/com/googlecode/n_orm/sample/businessmodel/BasicTest.java
+++ sample/src/test/java/com/googlecode/n_orm/sample/businessmodel/BasicTest.java
@@ -369,13 +369,15 @@ public class BasicTest extends HBaseTestLauncher {
 		if (! (ast instanceof Store)) //In case you've changed the default store
 			return;
 		Store st = (Store) ast;
-		//src/test/resources/com/googlecode/n_orm/sample/businessmodel/store.properties defines the compression property
-		assertEquals("gz", st.getCompression());
+		//According to src/test/resources/com/googlecode/n_orm/sample/businessmodel/store.properties, can be LZO (if available) or GZ
+		assertTrue(st.getCompression().equals("lzo") || st.getCompression().equals("gz"));
 		//Checking with HBase that the property column family is actually stored in GZ
 		HTableDescriptor td = st.getAdmin().getTableDescriptor(Bytes.toBytes(PersistingMixin.getInstance().getTable(Book.class) /*could be bsut.getTable()*/));
 		//Getting the property CF descriptor
 		HColumnDescriptor pd = td.getFamily(Bytes.toBytes(PropertyManagement.PROPERTY_COLUMNFAMILY_NAME));
-		assertEquals(Algorithm.GZ, pd.getCompression());
+		//According to src/test/resources/com/googlecode/n_orm/sample/businessmodel/store.properties, can be LZO (if available) or GZ
+		Algorithm comp = pd.getCompression();
+		assertTrue(comp.equals(Algorithm.LZO) || comp.equals(Algorithm.GZ));
 	 }
 
 }
