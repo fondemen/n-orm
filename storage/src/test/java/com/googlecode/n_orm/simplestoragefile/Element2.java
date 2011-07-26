8a261513fb10f0d270fa2cabe27f234652602589
f.fondement <f.fondement@localhost>
2011-07-26 09:53:04 +0200

    one store per file; no need for singleton at the store level

---
+++ storage/src/test/java/com/googlecode/n_orm/simplestoragefile/Element2.java
@@ -0,0 +1,9 @@
+package com.googlecode.n_orm.simplestoragefile;
+
+import com.googlecode.n_orm.Key;
+import com.googlecode.n_orm.Persisting;
+
+@Persisting
+public class Element2 {
+	@Key public byte key = '\0';
+}
