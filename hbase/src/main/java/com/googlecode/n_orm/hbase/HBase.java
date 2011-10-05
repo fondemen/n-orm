35f9b414be47fa27e2aaec8e4774d34f290d2e1e
f.fondement <none@none>
2011-10-05 15:40:27 +0200

    logging jars added to classpath

--- hbase/src/main/java/com/googlecode/n_orm/hbase/HBase.java
+++ hbase/src/main/java/com/googlecode/n_orm/hbase/HBase.java
@@ -68,7 +68,7 @@ public class HBase {
 						.getDeclaredMethod("addURL", parameters);
 				method.setAccessible(true);
 				method.invoke(sysloader, new Object[] { file.toURI().toURL() });
-				errorLogger.fine(file.getAbsolutePath() + " added to classpath");
+				logger.info(file.getAbsolutePath() + " added to classpath");
 			} catch (Throwable t) {
 				errorLogger.log(Level.SEVERE, "Warning: could not add jar file "	+ file.getAbsolutePath(), t);
 			}
