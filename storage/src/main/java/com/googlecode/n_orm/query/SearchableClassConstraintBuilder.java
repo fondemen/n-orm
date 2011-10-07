ab9a3844891cff368f0f52a8a046b41badf1a132
Théophile Helleboid <chtitux@gmail.com>
2011-10-07 15:57:22 +0200

    Implemenation of the seralization
    --HG--
    branch : pfe

--- storage/src/main/java/com/googlecode/n_orm/query/SearchableClassConstraintBuilder.java
+++ storage/src/main/java/com/googlecode/n_orm/query/SearchableClassConstraintBuilder.java
@@ -1,5 +1,8 @@
 package com.googlecode.n_orm.query;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.lang.reflect.Field;
 import java.util.NavigableSet;
 
@@ -178,5 +181,23 @@ public class SearchableClassConstraintBuilder<T extends PersistingElement>
 		else
 			limit = this.limit;
 		StorageManagement.processElementsRemotely(this.getClazz(), this.getConstraint(), action, callBack, limit, this.toBeActivated);
+	}
+	
+	/**
+	 * Serialize the elements in a OutputStream
+	 * @throws IOException 
+	 */
+	public void serialize(OutputStream out) throws IOException {
+		
+		ObjectOutputStream oos= new ObjectOutputStream(out);
+		Store s = StoreSelector.getInstance().getStoreFor(this.getClazz());
+		if (hasNoLimit())
+			throw new IllegalStateException("No limit set while store " + s + " for " + this.getClazz().getName() + " is not implementing " + ActionnableStore.class.getName() + " ; please use withAtMost expression.");
+		NavigableSet<T> set = StorageManagement.findElementsToSet(this.getClazz(), this.getConstraint(), this.limit, this.toBeActivated);
+
+		oos.writeObject(set);
+		
+		oos.flush();
+		oos.close();
 	}
 }
