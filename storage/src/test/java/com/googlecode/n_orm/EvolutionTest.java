039d5ff7ba34081962f379ad522c04240f0521a2
f.fondement <devnull@localhost>
2011-10-19 17:46:00 +0200

    An evolution test (new column familiy)

--- storage/src/test/java/com/googlecode/n_orm/EvolutionTest.java
+++ storage/src/test/java/com/googlecode/n_orm/EvolutionTest.java
@@ -3,8 +3,11 @@ package com.googlecode.n_orm;
 import static org.junit.Assert.*;
 
 import java.util.Date;
+import java.util.Set;
 
 import org.junit.Test;
+
+import com.googlecode.n_orm.cf.SetColumnFamily;
 
 public class EvolutionTest {
 	
@@ -48,5 +51,66 @@ public class EvolutionTest {
 		assertEquals(uninitialized.ival, v1.ival);
 		assertEquals(uninitialized.dval, v1.dval, Double.MIN_VALUE);
 		assertEquals(uninitialized.eval, v1.eval);
+		
+		v1.store();
+		assertTrue(v0.existsInStore());
+	}
+
+	@Persisting(table="evoltest")
+	public static class V0NewCf {
+		@Key public String key;
+		
+		public String aProp;
+		
+		public SetColumnFamily<String> cf = new SetColumnFamily<String>();
+	}
+	
+	@Test
+	public void newCf() {
+		String key = "JIOJJ:?IKBYI:NIUBYBF";
+		V0 v0 = new V0(); v0.key = key; v0.store();
+		
+		KeyManagement.getInstance().cleanupKnownPersistingElements();
+		
+		V0NewCf v1 = new V0NewCf();v1.key = key;
+		v1.activate("cf");
+		
+		assertTrue(v1.cf.isEmpty());
+
+		v1.aProp = "toti";
+		v1.cf.add("toto");
+		
+		v1.store();
+		assertTrue(v0.existsInStore());
+		
+		KeyManagement.getInstance().cleanupKnownPersistingElements();
+		
+		v1 = new V0NewCf();v1.key = key;
+		v1.activate("cf");
+		assertEquals("toti", v1.aProp);
+		assertEquals(1, v1.cf.size());
+		assertEquals("toto", v1.cf.iterator().next());
+	}
+	
+	@Persisting
+	public static class AnotherClass {
+		@Key public String key;
+		public String prop;
+		protected Set<String> cf = null;
+	}
+	
+	@Test
+	public void newCfSawnAfter() {
+		String key = "JIOJJ:?IKBYI:NIUBYBF";
+		AnotherClass sut = new AnotherClass();
+		sut.key = key;
+		sut.prop = "toto";
+		sut.store();
+		
+		AnotherClass sut2 = new AnotherClass();
+		sut2.key = key;
+		sut2.activate("cf");
+		
+		assertEquals("toto", sut2.prop);
 	}
 }
