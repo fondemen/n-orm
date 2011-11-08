00787d1667d3a796d06a3416cf1b78312ff22ef2
brundavid <none@none>
2011-11-08 14:28:28 +0100

    Add first JUnit tests
    --HG--
    branch : f-console

---
+++ f-console/src/test/java/com/googlecode/n_orm/f_console/commands/CommandListTest.java
@@ -0,0 +1,12 @@
+package com.googlecode.n_orm.f_console.commands;
+
+import org.junit.Test;
+
+public class CommandListTest
+{
+	@Test
+	public void exportTest()
+	{
+
+	}
+}
---
+++ f-console/src/test/java/com/googlecode/n_orm/f_console/f_shell/ShellTest.java
@@ -0,0 +1,6 @@
+package com.googlecode.n_orm.f_console.f_shell;
+
+public class ShellTest
+{
+
+}
