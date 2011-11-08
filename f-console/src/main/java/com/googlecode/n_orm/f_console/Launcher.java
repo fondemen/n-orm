1a8c7fe8452345d5f478aab62fddbb85ea31ce0c
brundavid <none@none>
2011-11-08 13:55:09 +0100

    Add java files from previous local project Add maven support for dependencies
    --HG--
    branch : f-console

---
+++ f-console/src/main/java/com/googlecode/n_orm/f_console/Launcher.java
@@ -0,0 +1,12 @@
+package com.googlecode.n_orm.f_console;
+
+import java.io.IOException;
+import com.googlecode.n_orm.f_console.f_shell.Shell;
+
+public class Launcher
+{
+	public static void main(String[] args) throws IOException
+	{
+		new Shell().launch();
+	}
+}
---
+++ f-console/src/main/java/com/googlecode/n_orm/f_console/commands/CommandList.java
@@ -0,0 +1,41 @@
+package com.googlecode.n_orm.f_console.commands;
+
+import org.codehaus.groovy.control.CompilationFailedException;
+
+import com.googlecode.n_orm.f_console.annotations.Trigger;
+import com.googlecode.n_orm.f_console.f_shell.Shell;
+
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
+
+public class CommandList
+{
+	private Shell shell;
+	
+	public CommandList(Shell shell)
+	{
+		this.shell = shell;
+	}
+	
+	@Trigger
+	public void export(String filePath)
+	{
+		shell.print("export command called successfully, param=" + filePath);
+	}
+	
+	@Trigger
+	public void changePrompt(String newPrompt)
+	{
+		shell.setPrompt(newPrompt.replaceAll("\\s+$", "") + " ");
+	}
+	
+	@Trigger
+	public void groovy() throws CompilationFailedException
+	{
+		Binding binding = new Binding();
+		binding.setVariable("val", new Integer(2));
+		GroovyShell shell = new GroovyShell(binding);
+		Object value = shell.evaluate("println 'Hello World!'; x = 123; return val * 10");
+		this.shell.print("val = " + value.toString());
+	}
+}
