d9218a488c1fdfcdbf9120e37e38ac7e52404b79
brundavid <brundavid@localhost>
2012-02-15 16:59:15 +0100

    Add a new annotaion to specify that a command is a Continuator Add a Continuator annotation to ConstraintBuilder

--- storage/src/main/java/com/googlecode/n_orm/query/ConstraintBuilder.java
+++ storage/src/main/java/com/googlecode/n_orm/query/ConstraintBuilder.java
@@ -1,10 +1,11 @@
 package com.googlecode.n_orm.query;
 
-
 import com.googlecode.n_orm.PersistingElement;
+import com.googlecode.n_orm.consoleannotations.Continuator;
 
 public class ConstraintBuilder {
 	
+	@Continuator
 	public <T extends PersistingElement> SearchableClassConstraintBuilder<T> ofClass(Class<T> clazz) {
 		return new SearchableClassConstraintBuilder<T>(clazz);
 	}
