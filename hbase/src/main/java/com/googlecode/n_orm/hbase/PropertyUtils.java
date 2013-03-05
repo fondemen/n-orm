79d08f7497773876e2bf44e5105d94aa300b168a
Frédéric Fondement <frederic.fondement@uha.fr>
2013-03-05 18:29:53 +0100

    Copy-on-write for HBase properties cache

--- hbase/src/main/java/com/googlecode/n_orm/hbase/PropertyUtils.java
+++ hbase/src/main/java/com/googlecode/n_orm/hbase/PropertyUtils.java
@@ -7,6 +7,7 @@ import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
@@ -88,24 +89,50 @@ public class PropertyUtils {
 
 	// A class to be used as the value for properties cache
 	private static class PropertyCacheValue {
-		private final Map<HBaseProperty<?>, Object> values = Collections.synchronizedMap(new TreeMap<HBaseProperty<?>, Object>(
-				new Comparator<HBaseProperty<?>>() {
+		// The value representing NULL to avoid double checking get and contains in the cache
+		private static final Object NULL_VALUE = new Object();
+		
+		// Comparator for cache sorted map
+		private static final Comparator<HBaseProperty<?>> comparator = new Comparator<HBaseProperty<?>>() {
 
-					@Override
-					public int compare(HBaseProperty<?> o1, HBaseProperty<?> o2) {
-						return o2.hashCode() - o1.hashCode();
-					}
-				}));
+			@Override
+			public int compare(HBaseProperty<?> o1, HBaseProperty<?> o2) {
+				return o2.hashCode() - o1.hashCode();
+			}
+		};
+		
+		// The cache for values
+		private volatile SortedMap<HBaseProperty<?>, Object> values = new TreeMap<HBaseProperty<?>, Object>(comparator);
 
 		public Object getValue(HBaseProperty<?> property, Store store,
 				Class<? extends PersistingElement> clazz, Field field,
 				String tablePostfix) {
+			// First checking cache (should not return null, but NULL_VALUE)
 			Object ret = this.values.get(property);
-			if (ret == null && !this.values.containsKey(property)) {
-				ret = property.getValueInt(store, clazz, field, tablePostfix);
-				this.values.put(property, ret);
+			assert ret != null : "Chached value other than NULL_VALUE";
+			if (ret == null) {
+				// Value does not exists
+				// Getting it and caching it
+				// Added to cache using copy-on-write
+				synchronized(this) {
+					// Double check in synchronized section
+					ret = this.values.get(property);
+					if (ret == null) {
+						// Yes, we really have to get that value
+						ret = property.getValueInt(store, clazz, field, tablePostfix);
+						// Not caching null value so that get immediately tells whether value exists in cache
+						if (ret == null)
+							ret = NULL_VALUE;
+						// Copy of cache
+						SortedMap<HBaseProperty<?>, Object> newValues = new TreeMap<HBaseProperty<?>, Object>(values);
+						// Adding valuue to cache
+						newValues.put(property, ret);
+						// Copy-on-write
+						this.values = newValues;
+					}
+				}
 			}
-			return ret;
+			return ret == NULL_VALUE ? null : ret;
 		}
 	}
 
