52043718a0f03463e0036416af0df4379b130ce6
Fr?d?ric Fondement <frederic.fondement@uha.fr>
2013-05-28 17:27:55 +0200

    corrected additional properties on element after deserialization

--- hbase/src/main/java/com/googlecode/n_orm/hbase/Store.java
+++ hbase/src/main/java/com/googlecode/n_orm/hbase/Store.java
@@ -11,7 +11,6 @@ import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
