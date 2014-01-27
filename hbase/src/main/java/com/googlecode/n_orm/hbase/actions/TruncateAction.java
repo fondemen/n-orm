79eabe128c8d87a75c7cfe6b6e844fe971a8196b
armellewamba <devnull@localhost>
2014-01-27 16:37:12 +0100

    implementations of truncate actions

--- hbase/src/main/java/com/googlecode/n_orm/hbase/actions/TruncateAction.java
+++ hbase/src/main/java/com/googlecode/n_orm/hbase/actions/TruncateAction.java
@@ -9,6 +9,7 @@ import org.apache.hadoop.mapreduce.Job;
 import org.hbase.async.DeleteRequest;
 import org.hbase.async.HBaseClient;
 import org.hbase.async.KeyValue;
+import org.hbase.async.Scanner;
 
 import com.googlecode.n_orm.DatabaseNotReachedException;
 import com.googlecode.n_orm.hbase.MangledTableName;
@@ -26,52 +27,87 @@ public class TruncateAction extends Action<Void> {
 		this.store = store;
 		this.scan = scan;
 	}
-	
-	protected void truncateSimple() throws IOException  {
-		ScanAction sc = new ScanAction(scan, tableName);
-		Scan r =null;
 
+	//
+	protected void truncateSimple() throws IOException {
+		Scanner r = null;
+		
 		try {
-			r = sc.getScan();
-	
-			final int nbRows = 100;
-		    ArrayList<KeyValue>  res = r.next(nbRows);
-			while (res != null && res.size() != 0) {
-				for ( KeyValue result : res) {
-					new DeleteRequest(tableName.getNameAsBytes(), result.key());
-				}
-				res = r.next(nbRows);
+			// initialisation du Scanner
+			if(this.scan.getFamily()!=null){
+				r.setFamily(this.scan.getFamily());
 			}
+			if(this.scan.getStartRow()!=null){
+				r.setStartKey(this.scan.getStartRow());
+			}
+			if(this.scan.getStopRow()!=null){
+				r.setStopKey(this.scan.getStopRow());
+			}
+			if(this.scan.getQualifier()!=null){
+				r.setQualifier(this.scan.getQualifier());
+			}
+			if(this.scan.getQualifiers()!=null){
+				r.setQualifiers(this.scan.getQualifiers());
+			}
+			
+			final int nbRows = 100;
+			DeleteAction da;
+			List<DeleteRequest> dels = new ArrayList<DeleteRequest>(nbRows);
+			Deferred<ArrayList<ArrayList<KeyValue>>> res = r.nextRows(nbRows);
+			ArrayList<ArrayList<KeyValue>> resultRow = res.join();
+			while (resultRow != null && resultRow.size() != 0) {
+				dels.clear();
+				for (ArrayList<KeyValue> r1 : resultRow) {
+					for (KeyValue kv : r1) {
+						dels.add(new DeleteRequest(tableName.getNameAsBytes(),
+								kv.key()));
+
+					}
+				}
+				for (DeleteRequest del : dels) {
+					da = new DeleteAction(del);
+				}
+
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} finally {
 			if (r != null)
 				r.close();
 		}
 	}
-	
-	protected void truncateMapReduce() throws IOException, InterruptedException, ClassNotFoundException  {
+
+	protected void truncateMapReduce() throws IOException,
+			InterruptedException, ClassNotFoundException {
 		String table_Name = tableName.toString();
-		Job count = Truncator.createSubmittableJob(this.store, table_Name, this.scan);
-		if(!count.waitForCompletion(false)) {
-			throw new DatabaseNotReachedException("Could not truncate table with map/reduce " + tableName);
+		Job count = Truncator.createSubmittableJob(this.store, table_Name,
+				this.scan);
+		if (!count.waitForCompletion(false)) {
+			throw new DatabaseNotReachedException(
+					"Could not truncate table with map/reduce " + tableName);
 		}
 	}
 
 	@Override
 	public Deferred<Void> perform(HBaseClient client) throws Exception {
-		if(this.store.isTruncateMapRed())
+		if (this.store.isTruncateMapRed())
 			this.truncateMapReduce();
 		else
 			this.truncateSimple();
 		return null;
 	}
-	
+
 	@Override
 	public MangledTableName getTable() {
 		return tableName;
 	}
-	
-	public void setTable(MangledTableName table){
-		this.tableName=table;
+
+	public void setTable(MangledTableName table) {
+		this.tableName = table;
 	}
 
 }
