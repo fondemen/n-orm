6062f02dec5381e83f12aaa8d854c6729c9573a7
armellewamba <devnull@localhost>
2014-02-08 03:21:18 +0100

    mocking test

---
+++ hbase/src/test/java/MockingTest/ActionTest.java
@@ -0,0 +1,13 @@
+package MockingTest;
+
+import static org.mockito.Mockito.*;
+
+import com.googlecode.n_orm.hbase.actions.GetAction;
+
+
+
+public class ActionTest {
+GetAction mockGetAction=mock(GetAction.class);
+	
+	
+}
